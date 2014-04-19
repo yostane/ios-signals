d3747b8c9b3839682cc343d2e86b80e9924a713a
yassine benabbas <yassine.benabbas@gmail.com>
2014-04-20 00:10:23 +0200

    Premier commit

---
+++ ios-signals/YOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YOSAppDelegate.h
+//  ios-isgnals
+//
+//  Created by yassine benabbas on 20/04/2014.
+//  Copyright (c) 2014 org.yostane.ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios-signals/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios-isgnals
+//
+//  Created by yassine benabbas on 20/04/2014.
+//  Copyright (c) 2014 org.yostane.ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YOSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YOSAppDelegate class]));
+    }
+}
